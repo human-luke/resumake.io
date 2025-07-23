1a4898627a0841fa74bcd1fa695f28ba34869bc3
human-luke <118990830+human-luke@users.noreply.github.com>
2025-07-23 16:48:46 +0200

    Update generate-source.ts

--- src/pages/api/generate-source.ts
+++ src/pages/api/generate-source.ts
@@ -40,7 +40,7 @@ function generateSourceCode(formData: FormValues) {
   if (opts.inputs) {
     zip.directory(opts.inputs.forEach((input: string) => {
   zip.directory(input, '../');
-  }
+  },
 
   zip.finalize()
 
